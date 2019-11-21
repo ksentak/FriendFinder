a097f4e73568aa8eb1874108e264ed847deee71d
KSentak <k.sentak@outlook.com>
2019-11-20 23:36:45 -0500

    Setip apiRoutes and ran initial program on local server

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,21 @@
+var friends = require("../data/friends");
+
+
+module.exports = function(app) {
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+  app.post("/api/friends", function(req, res) {
+      friends.push(req.body);
+      res.json(true);
+  });
+
+//   app.post("/api/clear", function(req, res) {
+//     // Empty out the arrays of data
+//     tableData.length = 0;
+//     waitListData.length = 0;
+
+//     res.json({ ok: true });
+//   });
+};
