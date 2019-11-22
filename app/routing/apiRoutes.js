c5304fcb0282fed297172b331f373135450fc445
KSentak <k.sentak@outlook.com>
2019-11-22 00:20:00 -0500

    Added for loops to loop through array of characters and find the best match to user

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,21 +6,37 @@ module.exports = function(app) {
   });
 
   app.post('/api/friends', function(req,res) {
-    var userScore = req.body.scores;
 
-    console.log(userScore);
-
+    var newFriend = req.body;
     
+    // ParseInt newFriend scores
+    for(var i = 0; i < newFriend.scores.length; i++) {
+      newFriend.scores[i] = parseInt(newFriend.scores[i]);
+    }
+
+    // Max Difference is defined as being the most points away and best friend has an identical score
+    var bestFriend = 0;
+    var maxDifference = 40;
+
+    for (var i = 0; i < friends.length; i++) {
+      var totalDifference = 0;
+      
+      for (var j = 0; j < friends[i].scores.length; j++) {
+        var difference = Math.abs(newFriend.scores[j] - friends[i].scores[j]);
+        totalDifference += difference;
+      }
+
+      if(totalDifference < maxDifference) {
+        bestFriend = i;
+        maxDifference = totalDifference;
+      }
+    }
+
+    friends.push(newFriend);
+
+    res.json(friends[bestFriend]);
+
+    console.log(bestFriend);
   });
+};
 
-  app.post("/api/clear", function(req, res) {
-    // Empty out the arrays of data
-    friends.length = 0;
-
-    res.json({ ok: true });
-  });
-
-
-
-
-}
